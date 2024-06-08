69a07f321f1c8e96a01c725feb4cea8abb1dbff9
Brown <memoyebrown@gmail.com>
2024-06-08 20:14:45 +0100

    remove unccessary fragment

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,66 +10,64 @@ const Dashboard = () => {
   const { profileOpen } = useLayoutContext();
 
   return (
-    <>
-      <main className="flex-1 py-10 lg:py-10">
-        <h1
-          className={`px-6 py-4 text-xl ${
-            profileOpen ? '-top-18' : 'sticky top-16 lgr:top-0'
-          } z-20 bg-white/80 font-bold text-foreground backdrop-blur-lg transition-[top] lg:text-3xl`}
-        >
-          Progress Dashboard
-        </h1>
+    <main className="flex-1 py-10 lg:py-10">
+      <h1
+        className={`px-6 py-4 text-xl ${
+          profileOpen ? '-top-18' : 'sticky top-16 lgr:top-0'
+        } z-20 bg-white/80 font-bold text-foreground backdrop-blur-lg transition-[top] lg:text-3xl`}
+      >
+        Progress Dashboard
+      </h1>
 
-        <div className="grid gap-4 px-6 lg:grid-cols-4">
-          <div className="col-span-2">
-            <CourseAssignment />
-          </div>
-          <div className="_max-xs:col-span-2 h-full">
-            <TodoItemCard
-              color="red"
-              title="Homework"
-              description="For today's lesson"
-              completed
-              time="35 MIN AGO"
-            />
-          </div>
-
-          <div className="_max-xs:col-span-2 h-full">
-            <TodoItemCard
-              color="blue"
-              title="Reading"
-              description="24 words per week"
-              completed
-              time="2H AGO"
-            />
-          </div>
-
-          <div className="col-span-2">
-            <GroupCard />
-          </div>
-
-          <div className="col-span-2">
-            <ProgressCard />
-          </div>
-
-          <div className="col-span-2">
-            <SmallCard
-              icon={PenIcon}
-              title="Grammer"
-              subtitle="+30 grammer rules"
-            />
-          </div>
-
-          <div className="col-span-2">
-            <SmallCard
-              icon={BookAIcon}
-              title="Dictionary"
-              subtitle="+10 new words"
-            />
-          </div>
+      <div className="grid gap-4 px-6 lg:grid-cols-4">
+        <div className="col-span-2">
+          <CourseAssignment />
         </div>
-      </main>
-    </>
+        <div className="_max-xs:col-span-2 h-full">
+          <TodoItemCard
+            color="red"
+            title="Homework"
+            description="For today's lesson"
+            completed
+            time="35 MIN AGO"
+          />
+        </div>
+
+        <div className="_max-xs:col-span-2 h-full">
+          <TodoItemCard
+            color="blue"
+            title="Reading"
+            description="24 words per week"
+            completed
+            time="2H AGO"
+          />
+        </div>
+
+        <div className="col-span-2">
+          <GroupCard />
+        </div>
+
+        <div className="col-span-2">
+          <ProgressCard />
+        </div>
+
+        <div className="col-span-2">
+          <SmallCard
+            icon={PenIcon}
+            title="Grammer"
+            subtitle="+30 grammer rules"
+          />
+        </div>
+
+        <div className="col-span-2">
+          <SmallCard
+            icon={BookAIcon}
+            title="Dictionary"
+            subtitle="+10 new words"
+          />
+        </div>
+      </div>
+    </main>
   );
 };
 
